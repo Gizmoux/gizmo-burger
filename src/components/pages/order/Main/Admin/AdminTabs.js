0e9e94f9b63c98e3ae1c2899c132c29369586b46
Vi Dev <vi@videv.fr>
2023-02-18 08:37:31 +0100

    ✨ F07: ADMIN > Tabs > Add modify (1e implementation)

--- src/components/pages/order/Main/Admin/AdminTabs.js
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -2,11 +2,28 @@ import styled from "styled-components"
 import Tab from "../../../../reusable-ui/Tab"
 import { FiChevronDown, FiChevronUp } from "react-icons/fi"
 import { AiOutlinePlus } from "react-icons/ai"
+import { MdModeEditOutline } from "react-icons/md"
 import { theme } from "../../../../../theme"
+import { useState } from "react"
 
 export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
+  const [isAddSelected, setIsAddSelected] = useState(true)
+  const [isEditSelected, setIsEditSelected] = useState(false)
+
   const handleClick = () => {
     setIsCollapsed(!isCollapsed)
+  }
+
+  const handleAddClick = () => {
+    setIsCollapsed(false)
+    setIsAddSelected(true)
+    setIsEditSelected(false)
+  }
+
+  const handleEditClick = () => {
+    setIsCollapsed(false)
+    setIsEditSelected(true)
+    setIsAddSelected(false)
   }
 
   return (
@@ -20,8 +37,14 @@ export default function AdminTabs({ isCollapsed, setIsCollapsed }) {
       <Tab
         label="Ajouter un produit"
         Icon={<AiOutlinePlus />}
-        onClick={handleClick}
-        className={isCollapsed ? "is-active" : ""}
+        onClick={handleAddClick}
+        className={isAddSelected ? "is-active" : ""}
+      />
+      <Tab
+        label="Modifier un produit"
+        Icon={<MdModeEditOutline />}
+        onClick={handleEditClick}
+        className={isEditSelected ? "is-active" : ""}
       />
     </AdminTabsStyled>
   )
