faf23db86524d609219f3f148d4ab8efd82d32eb
Vi Dev <vi@videv.fr>
2023-02-18 06:53:48 +0100

    💄 F07: ADMIN > Add conditional rendering (with useContext)

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,23 +1,31 @@
-import { useParams } from "react-router-dom"
+import { useState } from "react"
 import styled from "styled-components"
 import { theme } from "../../../theme"
 import Main from "./Main/Main"
 import Navbar from "./Navbar/Navbar"
+import OrderContext from "../../../context/OrderContext"
 
 export default function OrderPage() {
   // state
-  const { username } = useParams()
+  const [isModeAdmin, setIsModeAdmin] = useState(true)
 
   // comportements
 
+  const orderContextValue = {
+    isModeAdmin,
+    setIsModeAdmin,
+  }
+
   //affichage
   return (
-    <OrderPageStyled>
-      <div className="container">
-        <Navbar username={username} />
-        <Main />
-      </div>
-    </OrderPageStyled>
+    <OrderContext.Provider value={orderContextValue}>
+      <OrderPageStyled>
+        <div className="container">
+          <Navbar />
+          <Main />
+        </div>
+      </OrderPageStyled>
+    </OrderContext.Provider>
   )
 }
 
